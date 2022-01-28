c2cea6e7b7630b61db7f6e6b07faf5dd00796327
Aditya <likeadityakumar@gmail.com>
2022-01-28 15:56:24 +0530

    28th jan Commit

---
+++ Recursion/SkipCharacter.java
@@ -0,0 +1,19 @@
+package com.aditya.Recursion;
+
+public class SkipCharacter {
+    public static void main(String[] args) {
+        skip("","pawahegaheopaaaa");
+    }
+    static void skip(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        if(ch == 'a'){
+            skip(p,up.substring(1));
+        }else{
+            skip(p+ch,up.substring(1));
+        }
+    }
+}
